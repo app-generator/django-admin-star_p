d28754911a87bb51b2f3ca810b360e460e4a578b
App Generator <51070104+app-generator@users.noreply.github.com>
2022-12-12 10:33:29 +0200

    Release v1.0.0 - STABLE Release

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'admin_argon.apps.AdminArgonConfig',
+    'admin_star.apps.AdminStarConfig',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
